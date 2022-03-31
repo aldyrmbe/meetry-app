ac2e8e42ff7baf202e94331355d56a0da40348f2
Aldy Rambe <aldyrmbe@gmail.com>
2022-03-31 14:02:46 +0700

    Fix redirection

--- src/pages/peneliti/_middleware.tsx
+++ src/pages/peneliti/_middleware.tsx
@@ -5,6 +5,6 @@ import authenticate from "@lib/service/auth"
 export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
   const { role, authenticated } = await authenticate({ req, role: "PENELITI" })
   if (role && !authenticated)
-    return NextResponse.redirect(`${baseURL}/${role.toLowerCase()}/dashboard`)
-  if (!role && !authenticated) return NextResponse.redirect(baseURL)
+    return NextResponse.redirect(`${baseURL}/${role.toLowerCase()}/dashboard`.toString())
+  if (!role && !authenticated) return NextResponse.redirect(baseURL.toString())
 }
