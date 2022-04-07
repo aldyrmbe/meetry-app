80e9d3644537ebe24708bdd044cb27b6c4640537
Aldy Rambe <aldyrmbe@gmail.com>
2022-04-07 23:21:23 +0700

    Remove unused import

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps, LinkProps } from "@chakra-ui/react"
+import { Button, ButtonProps } from "@chakra-ui/react"
 
 const PrimaryButton = (props: ButtonProps) => {
   const { children, ...rest } = props
