5c706bef7809179b7822fd44f3e90a4b096b2248
Aldy Rambe <aldyrmbe@gmail.com>
2022-06-06 10:32:51 +0700

    Modify BE endpoints

--- src/constants/websocketUrl.ts
+++ src/constants/websocketUrl.ts
@@ -1,2 +1,2 @@
-export const WEB_SOCKET_URL = "http://meetry.azurewebsites.net/meetry-ws"
+export const WEB_SOCKET_URL = "https://meetry.azurewebsites.net/meetry-ws"
 // export const WEB_SOCKET_URL = "http://localhost:8080/meetry-ws"
